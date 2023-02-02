eece170f640c610e913472d0dd994a6dbce8179e
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-02-02 14:44:17 +0100

    add webapp

---
+++ webapp/src/App.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+
+import "./App.css";
+
+
+function App() {
+  const [file, setFile] = useState(null);
+  const [type, setType] = useState(null);
+  const [url, setUrl] = useState(null);
+  const [prediction, setPrediction] = useState(null);
+  const [feedback, setFeedback] = useState(null);
+
+  const UPLOAD_ENDPOINT =
+    "http://localhost:8064/upload-integration";
+
+  const uploadFile = async file => {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const str = JSON.stringify({version:"1.0.0", "type":type});
+    const bytes = new TextEncoder().encode(str);
+    const blob = new Blob([bytes], {
+      type: "application/json;charset=utf-8"
+    });
+    formData.append("settings", blob);
+
+    const response = fetch(UPLOAD_ENDPOINT, {
+      method: 'POST',
+      body:formData,
+      mode: 'cors'
+    });
+    const o = await response;
+    const u = await o.json();
+    return u;
+  };
+
+  const handleOnChange = async e => {
+    const file = e.target.files[0];
+    e.target.value = null;
+    setFile(file);
+    const objectURL = URL.createObjectURL(file);
+    setUrl(objectURL);
+    setPrediction(null);
+    setFeedback(null);
+    let res = await uploadFile(file);
+    setPrediction(res)
+  };
+
+  const handleFeedback = e => {
+    setFeedback(true);
+  };
+
+  const handleType = e => {
+    setType(e.target.value);
+    setPrediction(null);
+    setFeedback(null);
+    setFile(null);
+    setUrl(null);
+  }
+
+  return (
+      <>
+    <form >
+      <h1>Production environment</h1>
+      <input type="file" onChange={handleOnChange}  />
+      <select  value={type} onChange={handleType} >
+        <option value="pillow">Pillow</option>
+        <option value="opencv">Opencv</option>
+      </select>
+    </form>
+
+    { url && <img src={url} alt={"selected image"}  style={{maxWidth: "400px", maxHeight: "400px"}} />}
+        {prediction && <>
+          <p style={{fontSize:"2em", "padding": "0.4em", "margin": "0em", color:"white", "backgroundColor": "brown", "textAlign": "center"}}>It's a <b>{prediction.prediction}</b></p>
+
+
+          { feedback === true ? <p>Thank you!</p>  : <> <label>Do you agree with that result ?</label>
+        <button onClick={handleFeedback} type="button">Yes</button><button  onClick={handleFeedback} type="button">No</button>
+          </>}
+        </>
+        }
+
+
+        </>
+
+  );
+}
+
+export default App;
+
+
---
+++ webapp/src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {OidcProvider, OidcSecure} from "@axa-fr/react-oidc";
+
+const configuration = {
+ client_id: 'interactive.public.short',
+    redirect_uri: window.location.origin+'/authentication/callback',
+    silent_redirect_uri: window.location.origin+'/authentication/silent-callback',
+    //silent_login_uri: window.location.origin+'/authentication/silent-login',
+    scope: 'openid profile email api offline_access',
+    authority: 'https://demo.duendesoftware.com',
+    //authority_time_cache_wellknowurl_in_second: 60* 60,
+    refresh_time_before_tokens_expiration_in_second: 10,
+    service_worker_relative_url:'/OidcServiceWorker.js',
+    service_worker_only: false,
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <OidcProvider configuration={configuration} >
+<OidcSecure>
+          <App />
+  </OidcSecure>
+      </OidcProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
