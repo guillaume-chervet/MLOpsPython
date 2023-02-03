49add4e3fb1ef0b7d6a03427965a1d78c7bd896a
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-02-03 16:42:59 +0100

    sssss

--- demo_flake8.py
+++ demo_flake8.py
@@ -1,4 +1,5 @@
 print("hellor world hellor world hellor world hellor world hellor world hellor world hellor world hellor world hellor world")
 from pathlib import Path
 package_dir = Path(__file__).parent.absolute()
+
 print(package_dir)
