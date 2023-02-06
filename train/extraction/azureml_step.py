1a27da60743ab1b69579eaabca8dfdd4b283405e
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-02-06 21:06:40 +0100

    test

--- train/extraction/azureml_step.py
+++ train/extraction/azureml_step.py
@@ -1,6 +1,6 @@
 # mldesigner package contains the command_component which can be used to define component from a python function
 from mldesigner import command_component, Input, Output
-
+import mlflow
 
 
 @command_component(
@@ -12,5 +12,5 @@ def extraction_step(
     images_output: Output(type="uri_folder"),
 ):
     from extraction import extract_images
-
+    mlflow.log_metric("Final test loss", 0)
     extract_images(pdfs_input, images_output)
--- train/run_pipeline.py
+++ train/run_pipeline.py
@@ -3,7 +3,7 @@ from azure.identity import DefaultAzureCredential, InteractiveBrowserCredential
 
 from azure.ai.ml import MLClient, Input, Output
 from azure.ai.ml.dsl import pipeline
-
+from azure.ai.ml.constants import AssetTypes, InputOutputModes
 try:
     credential = DefaultAzureCredential()
     # Check if given credential can get token successfully.
@@ -58,12 +58,12 @@ def azureml_pipeline(input_data):
 
 pipeline_job = azureml_pipeline(
     input_data=Input(
-        path="azureml:cats_dogs_others:1", type="uri_folder"
+        path="azureml:cats_dogs_others:1", type=AssetTypes.URI_FOLDER
     )
 )
 # example how to change path of output on pipeline level
 pipeline_job.outputs.extraction_output = Output(
-    type="uri_folder", mode="rw_mount", path=custom_path
+    type=AssetTypes.URI_FOLDER, mode=InputOutputModes.RO_MOUNT, path=custom_path
 )
 
 pipeline_job = ml_client.jobs.create_or_update(
