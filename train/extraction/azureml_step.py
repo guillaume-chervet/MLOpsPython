514733e9e4f9864114d78dccabece77edd2cca06
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-02-06 17:06:06 +0100

    ssddssss

--- train/extraction/azureml_step.py
+++ train/extraction/azureml_step.py
@@ -1,7 +1,6 @@
 # mldesigner package contains the command_component which can be used to define component from a python function
 from mldesigner import command_component, Input, Output
 
-from .extraction import extract_images
 
 
 @command_component(
@@ -12,5 +11,6 @@ def extraction_step(
     pdfs_input: Input(type="uri_folder"),
     images_output: Output(type="uri_folder"),
 ):
+    from .extraction import extract_images
 
     extract_images(pdfs_input, images_output)
