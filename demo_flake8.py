845eaff80fc29a724749146c8a7339ef9945e603
Siremestir <swan.sauvegrain@gmail.com>
2023-02-13 13:27:02 +0100

    Initial commit

---
+++ demo_flake8.py
@@ -0,0 +1,7 @@
+print(
+    "hellor world hellor world hellor world hellor world hellor world hellor world hellor world hellor world hellor world"
+)
+from pathlib import Path
+
+package_dir = Path(__file__).parent.absolute()
+print(package_dir)
