1156d83d2e8de6cc0f3d1ebb68b389153bd348df
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-02-16 22:05:07 +0100

    add evaluation/scoring

--- train/evaluate/tests/evaluate_test.py
+++ train/evaluate/tests/evaluate_test.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import unittest
 from pathlib import Path
@@ -11,16 +12,27 @@ input_directory = BASE_PATH / "input"
 class EvaluateTest(unittest.TestCase):
 
     def test_something(self):
-
+        output_directory.mkdir(parents=True, exist_ok=True)
         model = Model(logging, str(input_directory / "final_model.h5"))
 
-        cats_directory = input_directory / "evaluate" / "cats"
-        for path in cats_directory.glob("*"):
+        results = []
+        cats_directory = input_directory / "evaluate"
+        for path in cats_directory.glob("**/*"):
+            if path.is_dir():
+                continue
             model_result = model.execute(str(path))
-            print(model_result)
 
+            prediction = model_result["prediction"]
+            prediction_truth = path.parent.name.lower().replace("s", "")
+            result = { "filepath": path.name,
+                       "ok": prediction_truth == prediction.lower(),
+                       "prediction": prediction,
+                       "values": model_result["values"] }
+            results.append(result)
 
-
+        with open( output_directory / "predictions.json", 'w') as file_stream:
+            json.dump(results, file_stream, indent=4)
+        
 
 if __name__ == '__main__':
     unittest.main()
