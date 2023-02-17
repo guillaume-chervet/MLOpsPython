97861f3c671fba11118515b86f4c480f5a3a1ad2
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-02-17 22:38:25 +0100

    add evaluation/scoring

--- train/evaluate/tests/evaluate_test.py
+++ train/evaluate/tests/evaluate_test.py
@@ -1,38 +1,28 @@
-import json
 import logging
+import shutil
 import unittest
 from pathlib import Path
 
-from model_pillow import Model
+from evaluate import evaluate
 
 BASE_PATH = Path(__file__).resolve().parent
 output_directory = BASE_PATH / "output"
 input_directory = BASE_PATH / "input"
 
+
 class EvaluateTest(unittest.TestCase):
 
-    def test_something(self):
+    def tearDown(self):
+        if output_directory.is_dir():
+            shutil.rmtree(str(output_directory))
+
+    def test_evaluate(self):
         output_directory.mkdir(parents=True, exist_ok=True)
-        model = Model(logging, str(input_directory / "final_model.h5"))
-
-        results = []
-        cats_directory = input_directory / "evaluate"
-        for path in cats_directory.glob("**/*"):
-            if path.is_dir():
-                continue
-            model_result = model.execute(str(path))
-
-            prediction = model_result["prediction"]
-            prediction_truth = path.parent.name.lower().replace("s", "")
-            result = { "filepath": path.name,
-                       "ok": prediction_truth == prediction.lower(),
-                       "prediction": prediction,
-                       "values": model_result["values"] }
-            results.append(result)
-
-        with open( output_directory / "predictions.json", 'w') as file_stream:
-            json.dump(results, file_stream, indent=4)
-        
+        model_output_directory = output_directory / "model"
+        model_output_directory.mkdir(parents=True, exist_ok=True)
+        integration_output_directory = output_directory / "integration"
+        integration_output_directory.mkdir(parents=True, exist_ok=True)
+        evaluate(logging, input_directory, model_output_directory, integration_output_directory)
 
 if __name__ == '__main__':
     unittest.main()
