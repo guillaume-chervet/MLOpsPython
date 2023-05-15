d4b88cb56e09303c048905f01841ece9c7258690
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-05-15 11:08:15 +0200

    test build a updatepackages

--- packages/inference/src/tests/inference_tests.py
+++ packages/inference/src/tests/inference_tests.py
@@ -12,9 +12,10 @@ class TestInference(unittest.TestCase):
 
     def test_todo(self):
         model = Model(logging, str(input_directory / "model" / "final_model.h5"))
-        model_result = model.execute(str(input_directory / "images" / "cat.png"))
-        print(model_result)
-        self.assertEqual(True, True)
+        inference_result = model.execute(str(input_directory / "images" / "cat.png"))
+
+        expected_result = {'prediction': 'Cat', 'values': [1.0, 2.370240289845506e-30, 0.0]}
+        self.assertEqual(inference_result, expected_result)
 
 
 if __name__ == "__main__":
