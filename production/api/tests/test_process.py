861059261a29e1a5c3f2da335be9fc720fd5c226
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-05-22 16:33:14 +0200

    [no ci] fix(production): update tests to output diff

--- production/api/tests/test_process.py
+++ production/api/tests/test_process.py
@@ -3,14 +3,14 @@ import unittest
 from pathlib import Path
 
 from ..core.base_app_settings import BaseAppSettings
-from ..core.model.model import Model
+from ..core.model.inference import Inference
 
 
 class TestProcess(unittest.IsolatedAsyncioTestCase):
 
     def test_process(self):
-        model = Model(logging, BaseAppSettings(logging))
-        dir = "./tests/gt/f2b68110-3b7b-4cc1-98fc-caf451d71ff2/mlcli"
+        model = Inference(logging, BaseAppSettings(logging))
+        dir = "./tests/cats-dogs-others/f2b68110-3b7b-4cc1-98fc-caf451d71ff2/mlcli"
         dest = "./tests/diff"
         flist = [p for p in Path(dir).iterdir() if p.is_file()]
         for fp in flist:
