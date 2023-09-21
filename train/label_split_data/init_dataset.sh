abaf107342d5cd53b10f8a734020d55ee77949e7
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-09-21 11:16:32 +0200

    Update init_dataset.sh

--- train/label_split_data/init_dataset.sh
+++ train/label_split_data/init_dataset.sh
@@ -12,7 +12,7 @@ else
   echo "Dataset not found so create it"
   git clone https://github.com/guillaume-chervet/dataset-cats-dogs-others-labels.git
   rm -rf dataset-cats-dogs-others-labels/.git
-  az ml data create -f dataset-cats-dogs-others.yml --resource-group $resource_group --workspace-name $workspace_name
+  az ml data create -f dataset-cats-dogs-others-label.yml --resource-group $resource_group --workspace-name $workspace_name
 
 fi
 
