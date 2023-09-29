c609d2a6d9962ffd27981d2e8177e6c5a2f69389
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-09-29 14:48:56 +0200

    feat(env): create infra automatically

--- train/evaluate/evaluate.py
+++ train/evaluate/evaluate.py
@@ -3,10 +3,12 @@ from pathlib import Path
 
 from mlopspython_inference.inference_pillow import Inference
 
+model_name = "final_model.keras"
 
-def evaluate(logging, input_model_directory: Path, input_images_directory: Path, output_directory: Path,
-             integration_output: Path):
-    model_path = input_model_directory / "final_model.keras"
+def evaluate(logging, input_model_directory: Path, input_images_directory: Path,
+             model_output_directory: Path,
+             integration_output_directory: Path):
+    model_path = input_model_directory / model_name
     model = Inference(logging, str(model_path))
 
     statistics = {
@@ -33,16 +35,16 @@ def evaluate(logging, input_model_directory: Path, input_images_directory: Path,
         results.append(result)
     statistics["total"] = statistics["ok"] + statistics["ko"]
 
-    with open(output_directory / "statistics.json", 'w') as file_stream:
+    with open(model_output_directory / "statistics.json", 'w') as file_stream:
         json.dump(statistics, file_stream, indent=4)
 
-    with open(output_directory / "predictions.json", 'w') as file_stream:
+    with open(model_output_directory / "predictions.json", 'w') as file_stream:
         json.dump(results, file_stream, indent=4)
 
-    model_path_output = output_directory / "final_model.h5"
+    model_path_output = model_output_directory / model_name
     model_path_output.write_bytes(model_path.read_bytes())
 
-    mlcli_directory = integration_output / "mlcli"
+    mlcli_directory = integration_output_directory / "mlcli"
     mlcli_directory.mkdir(parents=True, exist_ok=True)
     for source_path in tests_directory.glob("**/*"):
         if source_path.is_dir():
@@ -50,7 +52,7 @@ def evaluate(logging, input_model_directory: Path, input_images_directory: Path,
         destination_path = mlcli_directory / source_path.name
         destination_path.write_bytes(source_path.read_bytes())
 
-    ground_truth_directory = integration_output / "ground_truth"
+    ground_truth_directory = integration_output_directory / "ground_truth"
     ground_truth_directory.mkdir(parents=True, exist_ok=True)
     for file in results:
         filename = file["filename"]
