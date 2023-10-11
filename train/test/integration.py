e0b115d8f31ef2c4c0cc003da9210b25db8cff69
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-10-11 17:22:27 +0200

    uodate

---
+++ train/test/integration.py
@@ -0,0 +1,57 @@
+import json
+from pathlib import Path
+
+def prepare_integration_data(input_integration_directory, integration_output_directory,
+                             execute):
+
+    mlcli_directory = integration_output_directory / "mlcli"
+    mlcli_directory.mkdir(parents=True, exist_ok=True)
+    pdfs = [
+        p
+        for p in Path(input_integration_directory).iterdir()
+        if p.is_file() and p.suffix == ".pdf"
+    ]
+    ground_truth_directory = integration_output_directory / "ground_truth"
+    ground_truth_directory.mkdir(parents=True, exist_ok=True)
+    for pdf_source_path in pdfs:
+        destination_path = mlcli_directory / pdf_source_path.name
+        destination_path.write_bytes(pdf_source_path.read_bytes())
+        settings = {
+            "data": [
+                {"type": "file", "value": pdf_source_path.name},
+                {"key": "type", "value": "pillow"},
+            ]
+        }
+        with open(mlcli_directory / (pdf_source_path.stem + ".json"), "w") as file_stream:
+            json.dump(settings, file_stream, indent=4)
+    for pdf_source_path in pdfs:
+        images = [
+            p
+            for p in Path(input_integration_directory / pdf_source_path.stem).iterdir()
+            if p.is_file() and p.suffix == ".png"
+        ]
+        images.sort()
+        responses = []
+        for image_path in images:
+            model_result = execute(str(image_path))
+            responses.append(model_result)
+        data = {
+            "Url": "https://www.groundtruth.fr",
+            "FileName": pdf_source_path.name,
+            "FileDirectory": "",
+            "ImageDirectory": "",
+            "FrontDefaultStringsMatcher": "",
+            "StatusCode": 200,
+            "Body": json.dumps(responses),
+            "Headers": [],
+            "TimeMs": 0,
+            "TicksAt": 0,
+        }
+        truth_filename = (
+                Path(pdf_source_path).stem
+                + "_"
+                + Path(pdf_source_path).suffix.lower().replace(".", "")
+                + ".json"
+        )
+        with open(ground_truth_directory / truth_filename, "w") as file_stream:
+            json.dump(data, file_stream, indent=4)
--- train/test/test.py
+++ train/test/test.py
@@ -3,6 +3,8 @@ from pathlib import Path
 
 from mlopspython_inference.inference_pillow import Inference
 
+from integration import prepare_integration_data
+
 model_name = "final_model.keras"
 
 
@@ -44,63 +46,10 @@ def execute_model_and_generate_integration_test_data(
 
     with open(model_output_directory / "predictions.json", "w") as file_stream:
         json.dump(results, file_stream, indent=4)
-
     model_path_output = model_output_directory / model_name
     model_path_output.write_bytes(model_path.read_bytes())
 
-    mlcli_directory = integration_output_directory / "mlcli"
-    mlcli_directory.mkdir(parents=True, exist_ok=True)
-    pdfs = [
-        p
-        for p in Path(input_integration_directory).iterdir()
-        if p.is_file() and p.suffix == ".pdf"
-    ]
-
-    ground_truth_directory = integration_output_directory / "ground_truth"
-    ground_truth_directory.mkdir(parents=True, exist_ok=True)
-
-    for pdf_source_path in pdfs:
-        destination_path = mlcli_directory / pdf_source_path.name
-        destination_path.write_bytes(pdf_source_path.read_bytes())
-        settings = {
-            "data": [
-                {"type": "file", "value": pdf_source_path.name},
-                {"key": "type", "value": "pillow"},
-            ]
-        }
-        with open(model_output_directory / (pdf_source_path.stem + ".json"), "w") as file_stream:
-            json.dump(settings, file_stream, indent=4)
-
-    for pdf_source_path in pdfs:
-        images = [
-            p
-            for p in Path(input_integration_directory / pdf_source_path.stem).iterdir()
-            if p.is_file() and p.suffix == ".png"
-        ]
-        images.sort()
-        responses = []
-        for image_path in images:
-            model_result = model.execute(str(image_path))
-            responses.append(model_result)
-        data = {
-            "Url": "https://www.groundtruth.fr",
-            "FileName": pdf_source_path.name,
-            "FileDirectory": "",
-            "ImageDirectory": "",
-            "FrontDefaultStringsMatcher": "",
-            "StatusCode": 200,
-            "Body": json.dumps(responses),
-            "Headers": [],
-            "TimeMs": 0,
-            "TicksAt": 0,
-        }
-        truth_filename = (
-            Path(pdf_source_path).stem
-            + "_"
-            + Path(pdf_source_path).suffix.lower().replace(".", "")
-            + ".json"
-        )
-        with open(ground_truth_directory / truth_filename, "w") as file_stream:
-            json.dump(data, file_stream, indent=4)
+    prepare_integration_data(input_integration_directory, integration_output_directory, model.execute)
 
     return statistics
+
