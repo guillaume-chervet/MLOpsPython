a8dcd520321b28784b6cfa1acf70978db4768085
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-12-15 15:18:33 +0100

    feat(workshop): clean up for workshop

--- train/azureml_run_pipeline.py
+++ train/azureml_run_pipeline.py
@@ -9,6 +9,8 @@ from azure.ai.ml.constants import AssetTypes
 from azure.ai.ml.entities import Data
 from azure.ai.ml.entities import AmlCompute
 
+from extraction import register_extracted_dataset
+
 import uuid
 
 import json
@@ -17,13 +19,15 @@ parser = argparse.ArgumentParser("train")
 parser.add_argument("--subscription_id", type=str)
 parser.add_argument("--resource_group_name", type=str)
 parser.add_argument("--workspace_name", type=str)
+parser.add_argument("--location", type=str)
+parser.add_argument("--tags", type=str, default="{}")
 
 args = parser.parse_args()
 subscription_id = args.subscription_id
 resource_group_name = args.resource_group_name
 workspace_name = args.workspace_name
-
-URI_FOLDER = "uri_folder"
+location = args.location
+tags = json.loads(args.tags)
 
 try:
     credential = DefaultAzureCredential()
@@ -33,7 +37,6 @@ except Exception as ex:
     print(ex)
     # Fall back to InteractiveBrowserCredential in case DefaultAzureCredential not work
     credential = InteractiveBrowserCredential()
-
 
 
 # Get a handle to workspace
@@ -51,7 +54,7 @@ cluster_basic = AmlCompute(
     name=cluster_name,
     type="amlcompute",
     size="Standard_D4s_v3",
-    location="northeurope", #az account list-locations -o table
+    location=location,  # az account list-locations -o table
     min_instances=0,
     max_instances=1,
     idle_time_before_scale_down=60,
@@ -60,57 +63,70 @@ ml_client.begin_create_or_update(cluster_basic).result()
 
 
 @pipeline(default_compute=cluster_name)
-def azureml_pipeline(pdfs_input_data: Input(type=URI_FOLDER),
-                     labels_input_data: Input(type=URI_FOLDER)):
+def azureml_pipeline(
+    pdfs_input_data: Input(type=AssetTypes.URI_FOLDER),
+    labels_input_data: Input(type=AssetTypes.URI_FOLDER)
+):
     extraction_step = load_component(source="extraction/command.yaml")
-    extraction = extraction_step(
-        pdfs_input=pdfs_input_data
-    )
+    extraction = extraction_step(pdfs_input=pdfs_input_data)
 
     label_split_data_step = load_component(source="label_split_data/command.yaml")
-    label_split_data = label_split_data_step(labels_input=labels_input_data,
-                                             pdfs_input=pdfs_input_data,
-                                             images_input=extraction.outputs.images_output)
+    label_split_data = label_split_data_step(
+        labels_input=labels_input_data,
+        pdfs_input=pdfs_input_data,
+        images_input=extraction.outputs.images_output,
+    )
 
     train_step = load_component(source="train/command.yaml")
     train_data = train_step(
-        split_images_input=label_split_data.outputs.split_images_output)
+        split_images_input=label_split_data.outputs.split_images_output
+    )
 
     test_step = load_component(source="test/command.yaml")
-    test_data = test_step(model_input=train_data.outputs.model_output,
-                          integration_input=label_split_data.outputs.split_integration_output,
-                          images_input=label_split_data.outputs.split_images_output)
+    test_data = test_step(
+        model_input=train_data.outputs.model_output,
+        integration_input=label_split_data.outputs.split_integration_output,
+        images_input=label_split_data.outputs.split_images_output,
+    )
 
     return {
         "extraction_output": extraction.outputs.images_output,
+        "extraction_hash_output": extraction.outputs.hash_output,
         "model_output": test_data.outputs.model_output,
         "integration_output": test_data.outputs.integration_output,
     }
 
 
 pipeline_job = azureml_pipeline(
-    pdfs_input_data=Input(
-        path="azureml:cats_dogs_others:1", type=URI_FOLDER
-    ),
-    labels_input_data=Input(
-        path="azureml:cats_dogs_others_labels:1", type=URI_FOLDER
-    )
+    pdfs_input_data=Input(path="azureml:cats_dogs_others:1", type=AssetTypes.URI_FOLDER),
+    labels_input_data=Input(path="azureml:cats_dogs_others_labels:1", type=AssetTypes.URI_FOLDER),
 )
 
 
 azure_blob = "azureml://datastores/workspaceblobstore/paths/"
 experiment_id = str(uuid.uuid4())
-custom_extraction_path = azure_blob + "extraction/cats-dogs-others/" + experiment_id + "/"
-pipeline_job.outputs.model_output = Output(
-    type=URI_FOLDER, mode="rw_mount", path=custom_extraction_path
+custom_extraction_path = (
+    azure_blob + "extraction/cats-dogs-others/" + experiment_id + "/"
 )
+pipeline_job.outputs.model_output = Output(
+    type=AssetTypes.URI_FOLDER, mode="rw_mount", path=custom_extraction_path
+)
+custom_extraction_hash_path = (
+    azure_blob + "extraction_hash/cats-dogs-others/" + experiment_id + "/"
+)
+pipeline_job.outputs.extraction_hash_output = Output(
+    type=AssetTypes.URI_FOLDER, mode="rw_mount", path=custom_extraction_hash_path
+)
+
 custom_model_path = azure_blob + "models/cats-dogs-others/" + experiment_id + "/"
 pipeline_job.outputs.model_output = Output(
-    type=URI_FOLDER, mode="rw_mount", path=custom_model_path
+    type=AssetTypes.URI_FOLDER, mode="rw_mount", path=custom_model_path
 )
-custom_integration_path = azure_blob + "/integration/cats-dogs-others/" + experiment_id + "/"
+custom_integration_path = (
+    azure_blob + "/integration/cats-dogs-others/" + experiment_id + "/"
+)
 pipeline_job.outputs.integration_output = Output(
-    type=URI_FOLDER, mode="rw_mount", path=custom_integration_path
+    type=AssetTypes.URI_FOLDER, mode="rw_mount", path=custom_integration_path
 )
 
 pipeline_job = ml_client.jobs.create_or_update(
@@ -121,19 +137,9 @@ ml_client.jobs.stream(pipeline_job.name)
 
 credential.get_token("https://management.azure.com/.default")
 
-integration_dataset = Data(
-    name="cats-dogs-others-extraction",
-    path=custom_integration_path,
-    type=URI_FOLDER,
-    description="Extracted images for cats and dogs and others",
-    version="1",
-    tags={"source_type": "web", "source": "UCI ML Repo"},
+register_extracted_dataset(
+    ml_client, custom_extraction_hash_path, custom_extraction_path, {}
 )
-integration_dataset = ml_client.data.create_or_update(integration_dataset)
-print(
-    f"Dataset with name {integration_dataset.name} was registered to workspace, the dataset version is {integration_dataset.version}"
-)
-
 
 model_name = "cats-dogs-others"
 try:
@@ -142,22 +148,25 @@ except:
     model_version = "1"
 
 file_model = Model(
-        version=model_version,
-        path=custom_model_path,
-        type=AssetTypes.CUSTOM_MODEL,
-        name=model_name,
-        description="Model created from azureML.",
-    )
+    version=model_version,
+    path=custom_model_path,
+    type=AssetTypes.CUSTOM_MODEL,
+    name=model_name,
+    description="Model created from azureML.",
+)
 saved_model = ml_client.models.create_or_update(file_model)
 
-print(f"Model with name {saved_model.name} was registered to workspace, the model version is {saved_model.version}.")
+print(
+    f"Model with name {saved_model.name} was registered to workspace, the model version is {saved_model.version}."
+)
 
+integration_dataset_name = "cats-dogs-others-integration"
 integration_dataset = Data(
     name="cats-dogs-others-integration",
     path=custom_integration_path,
-    type=URI_FOLDER,
+    type=AssetTypes.URI_FOLDER,
     description="Integration dataset for cats and dogs and others",
-    tags={"source_type": "web", "source": "UCI ML Repo"},
+    tags={**tags},
 )
 integration_dataset = ml_client.data.create_or_update(integration_dataset)
 print(
--- train/extraction/command.py
+++ train/extraction/command.py
@@ -1,15 +1,30 @@
 import argparse
+from pathlib import Path
+
+from directory_hash import hash_dir
 from mlopspython_extraction.extraction import extract_images
 import mlflow
 
 parser = argparse.ArgumentParser("extraction")
 parser.add_argument("--pdfs_input", type=str)
 parser.add_argument("--images_output", type=str)
+parser.add_argument("--hash_output", type=str)
 
 args = parser.parse_args()
 pdfs_input = args.pdfs_input
 images_output = args.images_output
+hash_output = args.hash_output
 
 result = extract_images(pdfs_input, images_output)
+
+computed_hash = hash_dir(images_output)
+with open(str(Path(hash_output) / "hash.txt"), "w") as file:
+    file.write(computed_hash)
+
+console_output = f"""
+    number_files_input: {result.number_files_input}
+    number_images_output: {result.number_images_output}
+    computed_hash: {computed_hash}"""
+    
 mlflow.log_metric("number_files_input", result.number_files_input)
 mlflow.log_metric("number_images_output", result.number_images_output)
