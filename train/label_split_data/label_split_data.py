28ffbd41141805cf27ee6c613c64d239bb056431
Guillaume Chervet <guillaume.chervet@gmail.com>
2024-01-04 12:22:28 +0100

    fix(label_split_data): add suffle

--- train/label_split_data/label_split_data.py
+++ train/label_split_data/label_split_data.py
@@ -18,25 +18,33 @@ class LabelSplitDataResult:
     number_labeled_data: int
 
 
-def label_split_data(input_labels_path: Path,
-                     input_images_directory: Path,
-                     input_pdfs_directory: Path,
-                     output_images_directory: Path,
-                     output_integration_directory: Path,
-                     number_image_by_label=3,
-                     number_pdfs_integration=100,
-                     ratio_number_train_image: float = 0.4,
-                     ratio_number_test_image: float = 0.4,
-                     ) -> LabelSplitDataResult:
-
+def label_split_data(
+    input_labels_path: Path,
+    input_images_directory: Path,
+    input_pdfs_directory: Path,
+    output_images_directory: Path,
+    output_integration_directory: Path,
+    number_image_by_label=3,
+    number_pdfs_integration=100,
+    ratio_number_train_image: float = 0.4,
+    ratio_number_test_image: float = 0.4,
+) -> LabelSplitDataResult:
     if ratio_number_test_image + ratio_number_test_image > 1:
         raise Exception("sum of ratio must be inferior or equal to 1")
 
     Path(output_images_directory).mkdir(parents=True, exist_ok=True)
     Path(output_integration_directory).mkdir(parents=True, exist_ok=True)
-    pdfs = [p for p in Path(input_pdfs_directory).iterdir() if p.is_file() and p.suffix == ".pdf"]
+    pdfs = [
+        p
+        for p in Path(input_pdfs_directory).iterdir()
+        if p.is_file() and p.suffix == ".pdf"
+    ]
     random.shuffle(pdfs)
-    images = [p for p in Path(input_images_directory).iterdir() if p.is_file() and p.suffix == ".png"]
+    images = [
+        p
+        for p in Path(input_images_directory).iterdir()
+        if p.is_file() and p.suffix == ".png"
+    ]
     if len(pdfs) > number_pdfs_integration:
         pdfs = pdfs[:number_pdfs_integration]
     pdf_output_directory = output_integration_directory
@@ -49,7 +57,7 @@ def label_split_data(input_labels_path: Path,
     labels = ["cat", "dog", "other"]
     split_directory_names = ["train", "test", "evaluate"]
     annotations = label_data["annotations"]
-    annotations.suffle()
+    random.shuffle(annotations)
     for annotation in annotations:
         filename = annotation["fileName"]
         label = annotation["annotation"]["label"]
@@ -63,9 +71,14 @@ def label_split_data(input_labels_path: Path,
         if number_image_by_label != len(split_paths[label]):
             raise Exception("Not enough files for label " + label)
 
-        splitted = np.split(split_paths[label], [number_file_train, number_file_test + number_file_train])
+        splitted = np.split(
+            split_paths[label],
+            [number_file_train, number_file_test + number_file_train],
+        )
         for split_directory_name in split_directory_names:
-            labels_directory = output_images_directory / split_directory_name / (label + "s")
+            labels_directory = (
+                output_images_directory / split_directory_name / (label + "s")
+            )
             Path(labels_directory).mkdir(parents=True, exist_ok=True)
             for filename in splitted[split_directory_names.index(split_directory_name)]:
                 image_pdf_name = filename.split("_")[0]
@@ -75,15 +88,20 @@ def label_split_data(input_labels_path: Path,
                 destination_path = labels_directory / output_filename
                 source_path = input_images_directory / filename
                 destination_path.write_bytes(source_path.read_bytes())
-                path_result = split_directory_name + "/" + label + "s" + "/" + output_filename
+                path_result = (
+                    split_directory_name + "/" + label + "s" + "/" + output_filename
+                )
                 path_results.append(path_result)
 
     return LabelSplitDataResult(
         number_file_train_by_label=number_file_train,
         number_file_test_by_label=number_file_test,
-        number_file_evaluate_by_label=number_image_by_label - number_file_train - number_file_test,
+        number_file_evaluate_by_label=number_image_by_label
+        - number_file_train
+        - number_file_test,
         path_results=path_results,
-        number_labeled_data=len(annotations))
+        number_labeled_data=len(annotations),
+    )
 
 
 def copy_pdfs_integration(images, pdf_output_directory, pdfs):
