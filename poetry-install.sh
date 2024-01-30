3624fef2f8544beccbd0e2b9a2da93031a9e7690
Guillaume Chervet <guillaume.chervet@gmail.com>
2024-01-30 23:03:29 +0100

    feat(workshop): update for next course

--- poetry-install.sh
+++ poetry-install.sh
@@ -27,10 +27,6 @@ cd train
 poetry install --no-root
 cd $cwd
 
-cd train/labelling
-poetry install --no-root
-cd $cwd
-
 cd train/extraction
 poetry install --no-root
 poetry export --without-hashes --format=requirements.txt > requirements.txt
