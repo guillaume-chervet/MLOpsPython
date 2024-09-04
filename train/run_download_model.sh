5b91bb25bbee345c5397114a3a3f1f78a0c833c8
Guillaume Chervet <guillaume.chervet@gmail.com>
2024-09-04 13:26:59 +0200

    test

--- train/run_download_model.sh
+++ train/run_download_model.sh
@@ -5,4 +5,5 @@ AZURE_ML_WORKSPACE_NAME=$3
 az ml model download --name cats-dogs-others  \
     --version "$MODEL_VERSION"  \
     --resource-group "$AZURE_RESOURCE_GROUP_NAME" \
-    --workspace-name "$AZURE_ML_WORKSPACE_NAME"
+    --workspace-name "$AZURE_ML_WORKSPACE_NAME" \
+    --stage "Development"
