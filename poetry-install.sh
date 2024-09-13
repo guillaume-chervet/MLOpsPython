aefb264d41b3a312a5be4ea3062429933e65d474
Guillaume Chervet <guillaume.chervet@gmail.com>
2024-09-13 12:35:39 +0200

    refactor(docker): update docker

--- poetry-install.sh
+++ poetry-install.sh
@@ -1,27 +1,30 @@
-
+#!/bin/bash sh
 cwd=$(pwd)
 
-python -m pip install -e packages/inference
+function add_dependency {
+    package_name=$1
+    target_dir=$2
 
+    mkdir -p "$target_dir"
+    cp -R "$package_name" "$target_dir"
+}
+
+function add_dependencies {
+    add_dependency "packages/inference/" "production/api/packages"
+    add_dependency "packages/extraction/" "production/api/packages"
+}
+
+add_dependencies
+
+python -m pip install -e packages/inference
 cd packages/inference/
 poetry install
 poetry build
-poetry export --without-hashes --format=requirements.txt > requirements.txt
-cd dist
-cp *.whl ../../../train/test/packages
-cp *.whl ../../../production/api/packages
-cd $cwd
 
 python -m pip install -e packages/extraction
-
 cd packages/extraction/
 poetry install
 poetry build
-poetry export --without-hashes --format=requirements.txt > requirements.txt
-cd dist
-cp *.whl ../../../train/extraction/packages
-cp *.whl ../../../production/api/packages
-cd $cwd
 
 cd train
 poetry install --no-root
@@ -29,33 +32,23 @@ cd $cwd
 
 cd train/extraction
 poetry install --no-root
-poetry export --without-hashes --format=requirements.txt > requirements.txt
-sed -i 's#\(.*\)/packages/mlopspython_extraction-0.0.0-py3-none-any.whl\(.*\)#./packages/mlopspython_extraction-0.0.0-py3-none-any.whl\2#' requirements.txt
 
 cd $cwd
 cd train/test
 poetry install --no-root
-poetry export --without-hashes --format=requirements.txt > requirements.txt
-sed -i 's#\(.*\)/packages/mlopspython_inference-0.0.0-py3-none-any.whl\(.*\)#./packages/mlopspython_inference-0.0.0-py3-none-any.whl\2#' requirements.txt
 
 cd $cwd
 cd train/train
 poetry install --no-root
-poetry export --without-hashes --format=requirements.txt > requirements.txt
 
 cd $cwd
 cd train/label_split_data
 poetry install --no-root
-poetry export --without-hashes --format=requirements.txt > requirements.txt
 
 cd $cwd
 cd production/api
 poetry install --no-root
-poetry export --without-hashes --format=requirements.txt > requirements.txt
-sed -i 's#\(.*\)/packages/mlopspython_inference-0.0.0-py3-none-any.whl\(.*\)#./packages/mlopspython_inference-0.0.0-py3-none-any.whl\2#' requirements.txt
-sed -i 's#\(.*\)/packages/mlopspython_extraction-0.0.0-py3-none-any.whl\(.*\)#./packages/mlopspython_extraction-0.0.0-py3-none-any.whl\2#' requirements.txt
 
 cd $cwd
 cd production/integration
 poetry install --no-root
-poetry export --without-hashes --format=requirements.txt > requirements.txt
