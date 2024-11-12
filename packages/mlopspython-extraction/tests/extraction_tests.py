3ec154df57772d8586e6c55c9292b131db205425
Baptiste-Marin <baptistemv@gmail.com>
2024-11-12 15:40:26 +0100

    fix unittest extraction v10 - test updated

--- packages/mlopspython-extraction/tests/extraction_tests.py
+++ packages/mlopspython-extraction/tests/extraction_tests.py
@@ -27,9 +27,8 @@ class TestExtraction(unittest.TestCase):
         save_image_call = data_manager_mock.save_image.call_args_list
         print("[DEBUG] test_trailers_images_should_be_extracted------")
         print("[DEBUG] save_image_call: {0}".format(save_image_call))
-        self.assertTrue(save_image_call[0].args[1].endswith("_image0.jpg"))
-        self.assertTrue(save_image_call[1].args[1].endswith("_image50.jpg"))
-        self.assertTrue(save_image_call[2].args[1].endswith("_image100.jpg"))
+        self.assertTrue(save_image_call[0].args[1].endswith("_image0.png"))
+        self.assertEqual(len(save_image_call), 50 * len(data_manager_mock.get_movie_trailers(input_directory_str)))
 
 
 if __name__ == "__main__":
